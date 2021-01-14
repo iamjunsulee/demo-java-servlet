54ef9606221b2e8fab7da4f946747599e7749125
iamjunsulee <mellow0924@gmail.com>
2021-01-14 09:46:08 +0900

    Add dispatcherservlet setting

---
+++ src/main/java/me/junsu/SampleController.java
@@ -0,0 +1,21 @@
+package me.junsu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+    private final HelloService helloService;
+
+    public SampleController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hi")
+    @ResponseBody
+    public String getName() {
+        return "Hi, " + helloService.getName();
+    }
+}
