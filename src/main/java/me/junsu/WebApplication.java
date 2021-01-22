cf7eaff1ed7de778ee9474678880edc8d6befb3f
iamjunsulee <mellow0924@gmail.com>
2021-01-22 18:50:10 +0900

    Add WebApplication

---
+++ src/main/java/me/junsu/WebApplication.java
@@ -0,0 +1,23 @@
+package me.junsu;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebApplication implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+        webApplicationContext.register(WebConfig.class);
+        webApplicationContext.setServletContext(servletContext);
+        webApplicationContext.refresh();
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+        ServletRegistration.Dynamic app = servletContext.addServlet("app", dispatcherServlet);
+        app.addMapping("/app/*");
+    }
+}
--- src/main/java/me/junsu/WebConfig.java
+++ src/main/java/me/junsu/WebConfig.java
@@ -4,16 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(includeFilters = @ComponentScan.Filter(Controller.class))
-public class WebConfig {
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/", ".jsp");
     }
 }
